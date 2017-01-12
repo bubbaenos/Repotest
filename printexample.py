67d26b7581e67acb9a797de93aac26ae35a01a4d
bubbaenos <brianenos@comcast.net>
2017-01-11 22:09:02 -0800

    added

---
+++ printexample.py
@@ -0,0 +1,11 @@
+print('This is an example of print function.')
+print('We\'re going to the store.')
+print('Hi' +' there!')
+print ('This is eight as is ' + str(+8))
+print(1+3)
+
+exampleVar = print('whoa')
+
+example2Var = print('chichi')
+
+
